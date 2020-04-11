44ec61a9e839b2d42ceca2d12e0e1ac41f820120
Samuel Guevara <samsjamin@outlook.com>
2020-04-10 20:18:53 -0400

    Added css, js and html files.

---
+++ script.js
@@ -0,0 +1,19 @@
+var apiKey = '6b9dd327d5f0228f42ef746e3e9580c7' ;
+
+$(document).ready(function(){
+    
+    $('#submit').click(function(){
+
+        var city = $("#city").val();
+
+        if(city != ''){
+
+        }else{
+            $("#error").html('Field cannot be empty');
+        }
+    })
+
+
+
+
+});
