d336f3e6f977552ec46e4816d9969da47a8a2b84
Samuel Guevara <samsjamin@outlook.com>
2020-04-11 22:05:10 -0400

    input field is positioned absolute and so are the search results.

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 $(document).ready(function(){
-
+    
     $("#loader").hide();
-
-    $("form").on('submit', function(event){
+    
+    $("#searchBtn").on('click', function(event){
         event.preventDefault();
   
-        let query = $("#keyword").val()
+        let query = $("#searchquery").val()
         
-        let url = "api.openweathermap.org/data/2.5/forecast?q="+query+"&appid=6b9dd327d5f0228f42ef746e3e9580c7";
+        let url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=6b9dd327d5f0228f42ef746e3e9580c7";
+        
+        if (query !== ""){
+  
+            $.ajax({
+                url: url,
+                method: "GET",
+                dataType: "json",
+
+                beforeSend: function(){
+                    $("#loader").show();
+                  },
+                  complete: function(){
+                    $("#loader").hide();
+                },
+
+                success: function(main){
+                let output = "";
+                let weatherForecast = main;
+  
+                for(var x in weatherForecast){
+                    output +=`
+                    <div class="row">
+                    <div class="col s12 m5">
+                      <div class="card-panel teal">
+                        <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                    `
+                        ;
+                    }
+                    if(output !== ""){
+                        $("#weatherResults").html(output);
+  
+                    }else{
+
+                        let NoResults = "Sorry, there seems to be no results for - "+query+" -.";
+                        $("#weatherResults").html(NoResults);
+                    }
+                },
+                error: function(){
+                    console.log("error");
+                }
+            })
+  
+        }else{
+            console.log("Please enter something");
+        }
+  
+  
     });
   });
   
