dedbae021da75670d399c83c122b39b912355de6
Samuel Guevara <samsjamin@outlook.com>
2020-04-24 19:48:49 -0400

    updated to where only one search result appears.

--- README.md
+++ README.md
@@ -1,53 +1,38 @@
-# weather-schedule
-making a 5 day weather forecast
+# 05 Third-Party APIs: Work Day Scheduler
 
-# 06 Server-Side APIs: Weather Dashboard
+Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
-
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
 
 ## User Story
 
 ```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
 ```
 
 ## Acceptance Criteria
 
 ```
-GIVEN a weather dashboard with form inputs
-
-WHEN I search for a city
-
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-WHEN I view current weather conditions for that city
-
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-WHEN I view the UV index
-
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-WHEN I view future weather conditions for that city
-
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-
-WHEN I click on a city in the search history
-
-THEN I am again presented with current and future conditions for that city
-
-WHEN I open the weather dashboard
-
-THEN I am presented with the last searched city forecast
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
 ```
 
-The following image demonstrates the application functionality:
+The following animation demonstrates the application functionality:
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
+![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
 
 ## Review
 
--- script.js
+++ script.js
@@ -2,14 +2,44 @@ $(document).ready(function(){
     
     $("#loader").hide();
     
+    let searchRslts = [];
+
     $("#searchBtn").on('click', function(event){
         event.preventDefault();
   
         let query = $("#searchquery").val()
         
-        let remove_space = query.replace(/ /g,'+');
+        let remove_space = query.replace(/ /g,'')
 
-        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${remove_space}&units=imperial&appid=6b9dd327d5f0228f42ef746e3e9580c7`;
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${remove_space}&units=imperial&cnt=6&appid=6b9dd327d5f0228f42ef746e3e9580c7`;
+        
+        function getDate(){
+            var months = ["Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            
+            var days = new Array(7);
+            days[0] = "Sun";
+            days[1] = "Mon";
+            days[2] = "Tues";
+            days[3] = "Wed";
+            days[4] = "Thurs";
+            days[5] = "Fri";
+            days[6] = "Sat";
+
+            var today = new Date();
+            var dd    = String(today.getDate()).padStart(2, '0');
+            var nd    = days[today.getDay()];
+            var mm    = months[today.getMonth()];
+            var yyyy  = today.getFullYear();;
+
+            today = nd;
+            return today;
+          }
+
+        function tomorrowsDate(){
+          var today = new Date();
+          var tomorrowsDate = (my_date .getDate()+1)
+          return tomorrowsDate;
+        }
         
         if (remove_space !== ""){
   
@@ -28,24 +58,57 @@ $(document).ready(function(){
                 success: function(data){
                 let output = "";
                 let weatherForecast = data.list;
-                let weatherMain = data.list.main;
 
-
+                
+                
                 for(var x in weatherForecast){
                     output +=`
                     <div class="row">
-                    <div class="col s12 m5">
-                      <div class="card-panel blue-grey darken-4">
-                        <span class="white-text">${weatherForecast[x].main.temp}</span>
-                        <br>
-                        <span class="white-text">${weatherForecast[x].weather.description}</span>
-                        <br>
-                        <span class="white-text">${weatherForecast[x].dt}</span>
+                    <div class="column left">
+                        <p>${getDate()}</p>
+                        <h6>${data.city.name}, ${data.city.country}</h6>
+                        <p id="weatherTemp"><strong>${weatherForecast[x].main.temp}<sup>o</sup>F</strong></p>
+                        <p><img src="http://openweathermap.org/img/wn/${weatherForecast[0].weather[0].icon}@2x.png" alt="weather icon"></p>
+                        <p>Humidity: ${weatherForecast[x].main.humidity}</p>
+                        <p>Wind Speed: ${weatherForecast[0].wind.speed}</p>
+                        <p>UV Index</p>
+                    </div>
+                    <div class="column right cgrey last-column">
+                        <p class="dow">Thur</p>
+                        <p>${weatherForecast[5].main.temp}<sup>o</sup>F</p>
+                        <p><img src="http://openweathermap.org/img/wn/${weatherForecast[5].weather[0].icon}@2x.png" alt="weather icon"></p>
+                        <p>${weatherForecast[5].weather[0].main}</p>
+                        <p>Humidity</p>
                       </div>
-                    </div>
-                    </div>
-                    `
-                        ;
+                      <div class="column right">
+                        <p class="dow">Wed</p>
+                        <p>${weatherForecast[4].main.temp}<sup>o</sup>F</p>
+                        <p><img src="http://openweathermap.org/img/wn/${weatherForecast[4].weather[0].icon}@2x.png" alt="weather icon"></p>
+                        <p>${weatherForecast[4].weather[0].main}</p>
+                        <p>Humidity</p>
+                      </div>
+                      <div class="column right cgrey">
+                        <p class="dow">Tue</p>
+                        <p>${weatherForecast[3].main.temp}<sup>o</sup>F</p>
+                        <p><img src="http://openweathermap.org/img/wn/${weatherForecast[3].weather[0].icon}@2x.png" alt="weather icon"></p>
+                        <p>${weatherForecast[3].weather[0].main}</p>
+                        <p>Humidity</p>
+                      </div>
+                      <div class="column right">
+                        <p class="dow">Mon</p>
+                        <p>${weatherForecast[2].main.temp}<sup>o</sup>F</p>
+                        <p><img src="http://openweathermap.org/img/wn/${weatherForecast[2].weather[0].icon}@2x.png" alt="weather icon"></p>
+                        <p>${weatherForecast[2].weather[0].main}</p>
+                        <p>Humidity</p>
+                      </div>
+                      <div class="column right cgrey">
+                        <p class="dow">Sat</p>
+                        <p>${weatherForecast[1].main.temp}<sup>o</sup>F</p>
+                        <p><img src="http://openweathermap.org/img/wn/${weatherForecast[1].weather[0].icon}@2x.png" alt="weather icon"></p>
+                        <p>${weatherForecast[1].weather[0].main}</p>
+                        <p>Humidity</p>
+                      </div>
+                    `;
                     }
                     if(output !== ""){
                         $("#weatherResults").html(output);
@@ -62,11 +125,14 @@ $(document).ready(function(){
             })
   
         }else{
-            console.log("Please enter something");
+            M.toast({html: 'Please Enter Something', classes: 'N/A transparent'});
         }
   
   
     });
   });
   
-  
+$("#weatherResults").contents().each(function (){
+    
+})
+
